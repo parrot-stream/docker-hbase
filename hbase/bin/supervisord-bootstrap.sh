0b582fac683405b2091d4a5c9466d359afd50d8e
Matteo Capitanio <matteo.capitanio@gmail.com>
2016-09-17 20:44:34 +0000

    Fix

--- hbase/bin/supervisord-bootstrap.sh
+++ hbase/bin/supervisord-bootstrap.sh
@@ -88,13 +88,11 @@ if [ $rc -ne 0 ]; then
 fi
 
 
-ip=`awk 'END{print $1}' /etc/hosts`
-
 echo -e "\n\n--------------------------------------------------------------------------------"
 echo -e "You can now access to the following HBase Web UIs:"
 echo -e ""
-echo -e "HBase Master           http://$ip:60010"
-echo -e "HBase Region Server    http://$ip:60030\n"
+echo -e "HBase Master           http://localhost:60010"
+echo -e "HBase Region Server    http://localhost:60030\n"
 echo -e "Mantainer: Matteo Capitanio <matteo.capitanio.gmail.com>"
 echo -e "--------------------------------------------------------------------------------\n\n"
 
