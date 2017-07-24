513db0d5a0b9558f041d54295d1090eeea80d49c
Matteo Capitanio <matteo.capitanio@cdp.it>
2017-07-24 10:58:42 +0200

    Changed parent image to parrostream/centos-openjdk

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,21 @@
-FROM ubuntu:16.04
-
-ENV HBASE_VER 1.2.0+cdh5.11.1
+FROM parrotstream/centos-openjdk
 
 MAINTAINER Matteo Capitanio <matteo.capitanio@gmail.com>
 
-ENV IMPALA_VER 2.8.0+cdh5.11.1
-ENV HADOOP_VER 2.6.0+cdh5.11.1
-
-ENV JAVA_HOME /usr/lib/jvm/java-1.8.0-openjdk-amd64/
-
 USER root
 
-WORKDIR /opt/docker
-
-RUN apt-get update -y
-RUN apt-get upgrade -y
-RUN apt-get install -y wget apt-transport-https python-setuptools openjdk-8-jdk apt-utils sudo
-RUN easy_install supervisor
-RUN wget http://archive.cloudera.com/cdh5/one-click-install/trusty/amd64/cdh5-repository_1.0_all.deb
-RUN dpkg -i cdh5-repository_1.0_all.deb
-RUN apt-get update -y
-RUN apt-get install -y --allow-unauthenticated hbase-master=$HBASE_VER* hbase-regionserver=$HBASE_VER* hbase-rest=$HBASE_VER* hbase-thrift=$HBASE_VER*
+ADD cloudera-cdh5.repo /etc/yum.repos.d/
+RUN rpm --import https://archive.cloudera.com/cdh5/redhat/5/x86_64/cdh/RPM-GPG-KEY-cloudera
+RUN yum install -y hbase-master hbase-regionserver hbase-rest hbase-thrift
+RUN yum clean all
 
 RUN groupadd supergroup; \    
     usermod -a -G supergroup hbase
 
 ADD etc/supervisord.conf /etc/
 ADD etc/hbase/conf/hbase-site.xml /etc/hbase/conf/
+
+WORKDIR /
 
 # Various helper scripts
 ADD bin/start-hbase.sh ./
--- bin/supervisord-bootstrap.sh
+++ bin/supervisord-bootstrap.sh
@@ -1,48 +1,5 @@
 #!/bin/bash
 
-#rm /etc/ssh/*key* 2> /dev/null
-#rm /root/.ssh/id_rsa 2> /dev/null
- 
-#ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_dsa_key; \
-#ssh-keygen -q -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key; \
-#ssh-keygen -q -N "" -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key; \
-#ssh-keygen -q -N "" -t ed25519 -f /etc/ssh/ssh_host_ed25519_key; \
-#ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa; \
-#cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
-
-#rm /tmp/*.pid 2> /dev/null
-
-#wait-for-it.sh zookeeper:2181 -t 40
-#rc=$?
-#if [ $rc -ne 0 ]; then
-#    echo -e "\n---------------------------------------"
-#    echo -e "      ZooKeeper not ready! Exiting..."
-#    echo -e "---------------------------------------"
-#    exit $rc
-#fi
-
-#wait-for-it.sh hadoop:8020 -t 120
-#rc=$?
-#if [ $rc -ne 0 ]; then
-#    echo -e "\n---------------------------------------"
-#    echo -e "      HDFS not ready! Exiting..."
-#    echo -e "---------------------------------------"
-#    exit $rc
-#fi
-
-#hdfs dfsadmin -safemode leave
-
-#supervisorctl start sshd
-
-#wait-for-it.sh localhost:22 -t 60
-#rc=$?
-#if [ $rc -ne 0 ]; then
-#    echo -e "\n---------------------------------------"
-#    echo -e "      SSH not ready! Exiting..."
-#    echo -e "---------------------------------------"
-#    exit $rc
-#fi
-
-/opt/docker/start-hbase.sh
+/start-hbase.sh
 
 
